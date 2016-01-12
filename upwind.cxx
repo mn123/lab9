9cb5e774a8ff4a268e06666d66712326b328b994
Matthias Neufeld <maneuf@pc10.pool.tp1.uni-duesseldorf.de>
2016-01-12 15:32:05 +0100

    lab9

--- upwind.cxx
+++ upwind.cxx
@@ -10,17 +10,19 @@ void writeToFile(const double* const u, const string s, const double dx,
                  const double xmin, const int N);
 void initialize(double* const u, const double dx, const double xmin,
                 const int N);
+void upwind(double* const u0, double* const u1, const double dx, const double dt, const double V, const int N);
+void ftcs(double* const u0, double* const u1, const double dx, const double dt, const double V, const int N);
 //---------------------------------------
 int main(){
 
-  const double tEnd = ;
-  const double V = ;
+  const double tEnd = 5;
+  const double V = 1;
 
-  const int N  = ;
+  const int N  = 256;
   const double xmin = -10;
   const double xmax =  10;
   const double dx = (xmax-xmin)/(N-1);
-  double dt = ;
+  double dt = dx/2;
   const int Na = 10; // Number of output files up to tEnd
   const int Nk = int(tEnd/Na/dt);
 
@@ -39,7 +41,13 @@ int main(){
    for(int j=0; j<Nk; j++){
 
       // Put call to step function here
-
+      upwind(u0,u1,dx,dt,V,N);
+      //ftcs(u0,u1,dx,dt,V,N);
+     
+      h = u0;
+      u0 = u1;
+      u1 = h;
+      
       // swap arrays u0 <-> u1,
       // however do not copy values, be more clever ;)
    }
@@ -78,4 +86,20 @@ void writeToFile(const double* const u, const string s, const double dx,
      out << x << "\t" << u[i] << endl;
    }
    out.close();
+}
+//--------------------------------------------------
+void upwind(double* const u0, double* const u1, const double dx, const double dt, const double V, const int N)
+{	
+  u1[0] = -V*dt*(u0[0])/dx+u0[0];
+  for(int i=1; i<N; i++)
+    u1[i] = -V*dt*(u0[i]-u0[i-1])/dx+u0[i];
+  
+}
+//--------------------------------------------------
+void ftcs(double* const u0, double* const u1, const double dx, const double dt, const double V, const int N)
+{	
+  u1[0] = -V*dt*(u0[1])/(2*dx)+u0[0];
+  for(int i=1; i<N; i++)
+    u1[i] = -V*dt*(u0[i+1]-u0[i-1])/(2*dx)+u0[i];
+  
 }
